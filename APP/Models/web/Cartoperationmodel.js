3de47d5917a64ced1ba7f72dfc4476ac9be402ff
bhupi1979 <bhupendrapaliwal1983@gmail.com>
2025-08-29 19:12:46 +0530

    thei caretetable change

--- APP/Models/web/Cartoperationmodel.js
+++ APP/Models/web/Cartoperationmodel.js
@@ -25,8 +25,35 @@ const userSchema = mngs.Schema({
                     type:Number
                 }
            }
-  ]
-
+  ],
+ username:{
+    type:String,
+    
+  },
+   mobilenumber:{
+    type:String,
+    
+  },
+   address:{
+    type:String,
+    
+  },
+  locality:{
+    type:String,
+    
+  },
+   gstnumber:{
+    type:String,
+    
+  },
+   paymentmode:{
+    type:String,
+    
+  },
+  paymentsettlement:{
+    type:String
+  }
 },  {timestamps:true})
+userSchema.index({ createdAt: 1 }, { expireAfterSeconds: 43200 * 60 });
 let Cartoperation=mngs.model('carttable',userSchema)
 module.exports=Cartoperation
